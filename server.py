706fdbecd021e99ebef53691d6b1d7005f4001c4
tylermlui <tylerlui@gmail.com>
2025-04-12 14:40:17 -0700

    test db

--- server.py
+++ server.py
@@ -1,20 +1,25 @@
 import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
-
+import psycopg2
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for
-from functools import wraps
 from flask import redirect, session, url_for
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
+USER = env.get("user")
+PASSWORD = env.get("password")
+HOST = env.get("host")
+PORT = env.get("pgport")
+DBNAME = env.get("dbname")
+
 app = Flask(__name__)
 app.secret_key = env.get("APP_SECRET_KEY")
-
+ 
 oauth = OAuth(app)
 
 oauth.register( # Registers this app to Auth0 using OAuth
@@ -69,7 +74,39 @@ def home():
 @app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template("dashboard.html",user=session["user"], pretty=json.dumps(session.get('user'), indent=4))
+    try:
+        print("Connecting with:", USER, HOST, DBNAME)
+        connection = psycopg2.connect(
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT,
+            dbname=DBNAME
+        )
+        print("Connection successful!")
+        
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+        
+        # Example query
+        user_id = session["user"]["userinfo"]["sub"]
+        cursor.execute(
+            "INSERT INTO account_balance (user_id) VALUES (%s)",
+            (user_id,)  # Safely insert user_id
+        )
+        cursor.execute("SELECT created_at, account_balance FROM users")
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
+
+    except Exception as e:
+        print(f"Failed to connect: {e}")
+
+    return render_template("dashboard.html", user=session["user"], pretty=json.dumps(session.get('user'), indent=4))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 3000))
